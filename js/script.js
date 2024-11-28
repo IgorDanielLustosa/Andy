8426e89bda3a2eb4391c802b3d33a48cd4246364
IgorDanielLustosa <105529100+IgorDanielLustosa@users.noreply.github.com>
2024-11-28 15:31:50 -0300

    Falta Aprovação.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+
+
 let menuToggle = document.querySelector('.menuToggle');
 let header = document.querySelector('header');
 let section = document.querySelector('section');
 // let section = document.getElementsByTagName('*');
 
-menuToggle.onclick = function(){
+
+menuToggle.onclick = function () {
     header.classList.toggle('active')
     section.classList.toggle('active')
 }
